53ca9e7c71017a34ce496df2054bc977d5b68b4f
IrinaBBB <balkarovairina@gmail.com>
2022-01-05 11:10:01 +0100

    add: API project

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public string GetProducts()
+    {
+        return "list";
+    }
+
+    [HttpGet("{id}")]
+    public string GetProduct(int id)
+    {
+        return $"one product   {id}";
+    }
+}
